01ec08c8ce659d707d579b68b6eefc923c9c2ca7
NagrajGowda97 <nagrajg1997@gmail.com>
2022-10-11 10:27:38 +0530

    Search for contact by city, state or zipcode

--- src/main/java/com/gowda/AddressBookApp.java
+++ src/main/java/com/gowda/AddressBookApp.java
@@ -7,21 +7,41 @@ public class AddressBookApp {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        ContactFunc func = new ContactFunc();
+        boolean exit = false;
 
-        System.out.println("Press 1 to add contact");
-        System.out.println("Press 2 to delete contact");
-        System.out.println("Press 3 to edit contact");
-        int num = sc.nextInt();
+        while (!exit) {
+            ContactFunc func = new ContactFunc();
+            System.out.println("""
+                     Press 1 to add contact
+                     Press 2 to delete contact
+                     Press 3 to edit contact
+                     Press 4 to add multiple contact
+                     press 5 to search contact by city, state or zip code
+                     press 6 to exit""");
+            int num = sc.nextInt();
 
-        switch (num){
-            case 1:func.addContact();
-            case 2:func.deleteContact();
-            case 3:func.editContact();
-
-            default:
+            switch (num) {
+                case 1:
+                    func.addContact();
+                    break;
+                case 2:
+                    func.deleteContact();
+                    break;
+                case 3:
+                    func.editContact();
+                    break;
+                case 4:
+                    func.multipleContact();
+                    break;
+                case 5:
+                    func.viewContactBy();
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+            }
+            func.display();
         }
-
-        func.display();
     }
 }
--- src/main/java/com/gowda/ContactFunc.java
+++ src/main/java/com/gowda/ContactFunc.java
@@ -8,30 +8,8 @@ public class ContactFunc {
     static List<ContactDetails> details = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
-    public  void addContact(){
-        ContactDetails contact = new ContactDetails();
-        System.out.println("Enter First Name");
-        contact.setFirstName(sc.next());
-        System.out.println("Enter Middle Name");
-        contact.setMiddleName(sc.next());
-        System.out.println("Enter Last Name");
-        contact.setLastName(sc.next());
-        System.out.println("Address");
-        contact.setAddress(sc.next());
-        System.out.println("City");
-        contact.setCity(sc.next());
-        System.out.println("State");
-        contact.setState(sc.next());
-        System.out.println("Zip code");
-        contact.setZipCode(sc.nextInt());
-        System.out.println("Phone Number");
-        contact.setPhNumber(sc.nextLong());
-        System.out.println("Email address");
-        contact.setMailId(sc.next());
+    public void addContact() {
 
-        details.add(contact);
-    }
-    public void deleteContact(){
         System.out.println("Enter First Name");
         String firstName = sc.next();
         System.out.println("Enter Middle Name");
@@ -39,29 +17,93 @@ public class ContactFunc {
         System.out.println("Enter Last Name");
         String lastName = sc.next();
 
-       // ContactDetails contact = new ContactDetails();
-        for (ContactDetails contact: details) {
-            if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)){
-                details.remove(contact);
+
+        //this for loop will check weather entered contact have any duplicate or not.
+        //if any duplicate is present then it will not continue further to save contact, if no duplicate is present it save contact.
+        for (ContactDetails contact : details) {
+            if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
+                System.out.println("Contact is already exist ");
+                return;
             }
-            else {
-                System.out.println("Contact not found");
-            }
+        }
+            ContactDetails contact = new ContactDetails();
+            contact.setFirstName(firstName);
+            contact.setMiddleName(middleName);
+            contact.setLastName(lastName);
+            System.out.println("Address");
+            contact.setAddress(sc.next());
+            System.out.println("City");
+            contact.setCity(sc.next());
+            System.out.println("State");
+            contact.setState(sc.next());
+            System.out.println("Zip code");
+            contact.setZipCode(sc.nextInt());
+            System.out.println("Phone Number");
+            contact.setPhNumber(sc.nextLong());
+            System.out.println("Email address");
+            contact.setMailId(sc.next());
+            details.add(contact);
 
         }
-    }
-    public void editContact(){
+        public void deleteContact() {
 
-        System.out.println("Enter First Name");
-        String firstName = sc.next();
-        System.out.println("Enter Middle Name");
-        String middleName = sc.next();
-        System.out.println("Enter Last Name");
-        String lastName = sc.next();
+            System.out.println("Enter First Name");
+            String firstName = sc.next();
+            System.out.println("Enter Middle Name");
+            String middleName = sc.next();
+            System.out.println("Enter Last Name");
+            String lastName = sc.next();
 
-        for (ContactDetails contact : details) {
+            for (ContactDetails contact : details) {
+                if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
+                    details.remove(contact);
+                } else {
+                    System.out.println("Contact not found");
+                }
+            }
+        }
+        public void editContact () {
 
-            if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)){
+            System.out.println("Enter First Name");
+            String firstName = sc.next();
+            System.out.println("Enter Middle Name");
+            String middleName = sc.next();
+            System.out.println("Enter Last Name");
+            String lastName = sc.next();
+
+            for (ContactDetails contact : details) {
+
+                if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
+                    System.out.println("Enter First Name");
+                    contact.setFirstName(sc.next());
+                    System.out.println("Enter Middle Name");
+                    contact.setMiddleName(sc.next());
+                    System.out.println("Enter Last Name");
+                    contact.setLastName(sc.next());
+                    System.out.println("Address");
+                    contact.setAddress(sc.next());
+                    System.out.println("City");
+                    contact.setCity(sc.next());
+                    System.out.println("State");
+                    contact.setState(sc.next());
+                    System.out.println("Zip code");
+                    contact.setZipCode(sc.nextInt());
+                    System.out.println("Phone Number");
+                    contact.setPhNumber(sc.nextLong());
+                    System.out.println("Email address");
+                    contact.setMailId(sc.next());
+
+                    details.add(contact);
+                } else {
+                    System.out.println("Contact not found");
+                }
+            }
+        }
+        public void multipleContact(){
+            System.out.println("Enter number of contact to add in contact list");
+            int num = sc.nextInt();
+            for (int i=0;i<num;i++){
+                ContactDetails contact = new ContactDetails();
                 System.out.println("Enter First Name");
                 contact.setFirstName(sc.next());
                 System.out.println("Enter Middle Name");
@@ -83,14 +125,56 @@ public class ContactFunc {
 
                 details.add(contact);
             }
-            else {
-                System.out.println("Contact not found");
+        }
+        public void viewCity(){
+            System.out.println("Enter city to view contact");
+            String city = sc.next();
+            for (ContactDetails contact : details) {
+                if (contact.getCity().toLowerCase().contains(city)){
+                    System.out.println(details);
+                }
+                System.out.println("No contact found in this city");
             }
         }
-    }
+        public void viewState(){
+            System.out.println("Enter city to view contact");
+            String state = sc.next();
+            for (ContactDetails contact : details) {
+                if (contact.getCity().toLowerCase().contains(state)){
+                    System.out.println(details);
+                }
+                System.out.println("No contact found in this state");
+            }
+        }
+        public void viewZipCode(){
+            System.out.println("Enter city to view contact");
+            int zipCode = sc.nextInt();
+            for (ContactDetails contact : details) {
+                if (zipCode == contact.getZipCode()){
+                    System.out.println(details);
+                }
+                System.out.println("No contact found in this zipcode");
+            }
+        }
+        public void viewContactBy(){
 
-    public void display(){
-        System.out.println(details);
-    }
+            System.out.println("""
+                    Press 1 to view Contact by city
+                    Press 2 to view Contact by state
+                    Press 3 to view Contact by zip code""");
+            int num = sc.nextInt();
+            switch (num){
+                case 1:viewCity();
+                    break;
+                case 2:viewState();
+                    break;
+                case 3:viewZipCode();
+                    break;
+            }
+
+        }
+        public void display () {
+            System.out.println(details);
+        }
 
 }
