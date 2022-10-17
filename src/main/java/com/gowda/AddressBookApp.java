4efbf232ebf4b2833c7c37b5a854b339934fda1a
NagrajGowda97 <nagrajg1997@gmail.com>
2022-10-17 19:35:50 +0530

    Added method for multiple address book by creating object reference

--- src/main/java/com/gowda/AddressBookApp.java
+++ src/main/java/com/gowda/AddressBookApp.java
@@ -3,21 +3,22 @@ package com.gowda;
 import java.util.Scanner;
 
 public class AddressBookApp {
-
     static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  {
+        ContactFunc func = new ContactFunc();
+        func.chooseAddressBook();
         boolean exit = false;
 
         while (!exit) {
-            ContactFunc func = new ContactFunc();
             System.out.println("""
                      Press 1 to add contact
                      Press 2 to delete contact
                      Press 3 to edit contact
                      Press 4 to add multiple contact
                      press 5 to search contact by city, state or zip code
-                     press 6 to exit""");
+                     Press 6 to add new address book or to search address book
+                     Press 7 to Show contact
+                     press 8 to exit""");
             int num = sc.nextInt();
 
             switch (num) {
@@ -37,6 +38,12 @@ public class AddressBookApp {
                     func.viewContactBy();
                     break;
                 case 6:
+                    func.chooseAddressBook();
+                    break;
+                case 7:
+                    func.showContacts(ContactFunc.currentAddressBook);
+                    break;
+                case 8:
                     exit = true;
                     break;
                 default:
--- src/main/java/com/gowda/ContactFunc.java
+++ src/main/java/com/gowda/ContactFunc.java
@@ -1,14 +1,19 @@
 package com.gowda;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class ContactFunc {
-    static List<ContactDetails> details = new ArrayList<>();
+    public static HashMap<String, ArrayList> addressBookList  = new HashMap<String, ArrayList>();
+    static ArrayList<ContactDetails> currentAddressBook;
+    static String addressBookName;
+    static String currentAddressBookName;
     static Scanner sc = new Scanner(System.in);
 
-    public void addContact() {
+    public void addContact(){
 
         System.out.println("Enter First Name");
         String firstName = sc.next();
@@ -16,11 +21,9 @@ public class ContactFunc {
         String middleName = sc.next();
         System.out.println("Enter Last Name");
         String lastName = sc.next();
-
-
         //this for loop will check weather entered contact have any duplicate or not.
         //if any duplicate is present then it will not continue further to save contact, if no duplicate is present it save contact.
-        for (ContactDetails contact : details) {
+        for (ContactDetails contact : currentAddressBook) {
             if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
                 System.out.println("Contact is already exist ");
                 return;
@@ -42,7 +45,7 @@ public class ContactFunc {
             contact.setPhNumber(sc.nextLong());
             System.out.println("Email address");
             contact.setMailId(sc.next());
-            details.add(contact);
+            currentAddressBook.add(contact);
 
         }
         public void deleteContact() {
@@ -53,10 +56,9 @@ public class ContactFunc {
             String middleName = sc.next();
             System.out.println("Enter Last Name");
             String lastName = sc.next();
-
-            for (ContactDetails contact : details) {
+            for (ContactDetails contact : currentAddressBook) {
                 if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
-                    details.remove(contact);
+                    currentAddressBook.remove(contact);
                 } else {
                     System.out.println("Contact not found");
                 }
@@ -71,7 +73,7 @@ public class ContactFunc {
             System.out.println("Enter Last Name");
             String lastName = sc.next();
 
-            for (ContactDetails contact : details) {
+            for (ContactDetails contact : currentAddressBook) {
 
                 if (contact.getFirstName().equalsIgnoreCase(firstName) && contact.getMiddleName().equalsIgnoreCase(middleName) && contact.getLastName().equalsIgnoreCase(lastName)) {
                     System.out.println("Enter First Name");
@@ -93,7 +95,7 @@ public class ContactFunc {
                     System.out.println("Email address");
                     contact.setMailId(sc.next());
 
-                    details.add(contact);
+                    currentAddressBook.add(contact);
                 } else {
                     System.out.println("Contact not found");
                 }
@@ -123,58 +125,109 @@ public class ContactFunc {
                 System.out.println("Email address");
                 contact.setMailId(sc.next());
 
-                details.add(contact);
+                currentAddressBook.add(contact);
             }
         }
-        public void viewCity(){
-            System.out.println("Enter city to view contact");
-            String city = sc.next();
-            for (ContactDetails contact : details) {
-                if (contact.getCity().toLowerCase().contains(city)){
-                    System.out.println(details);
+        public void viewCity(String city){
+            System.out.println("Search result: ");
+            for (String addressBookName : addressBookList.keySet()) {
+                for (Object p : addressBookList.get(addressBookName)) {
+                    ContactDetails person = (ContactDetails) p;
+                    if (person.getCity().equalsIgnoreCase(city)) {
+                        System.out.println(currentAddressBook);
+                    }
                 }
-                System.out.println("No contact found in this city");
             }
         }
-        public void viewState(){
-            System.out.println("Enter city to view contact");
-            String state = sc.next();
-            for (ContactDetails contact : details) {
-                if (contact.getCity().toLowerCase().contains(state)){
-                    System.out.println(details);
+        public void viewState(String state){
+            System.out.println("Search result: ");
+            for (String addressBookName : addressBookList.keySet()){
+                for (Object p : addressBookList.get(addressBookName)) {
+                    ContactDetails person = (ContactDetails) p;
+                    if (person.getCity().equalsIgnoreCase(state)) {
+                        System.out.println(currentAddressBook);
+                    }
                 }
-                System.out.println("No contact found in this state");
             }
         }
-        public void viewZipCode(){
-            System.out.println("Enter city to view contact");
-            int zipCode = sc.nextInt();
-            for (ContactDetails contact : details) {
-                if (zipCode == contact.getZipCode()){
-                    System.out.println(details);
+
+        public void viewZipCode(int zipCode){
+            System.out.println("Search result: ");
+            for (String addressBookName : addressBookList.keySet()) {
+                for (Object p : addressBookList.get(addressBookName)) {
+                    ContactDetails person = (ContactDetails) p;
+                    if (person.getZipCode() == zipCode) {
+                        System.out.println(currentAddressBook);
+                    }
                 }
-                System.out.println("No contact found in this zipcode");
             }
         }
         public void viewContactBy(){
-
             System.out.println("""
                     Press 1 to view Contact by city
                     Press 2 to view Contact by state
                     Press 3 to view Contact by zip code""");
             int num = sc.nextInt();
             switch (num){
-                case 1:viewCity();
+                case 1:
+                    System.out.println("Enter city name: ");
+                    viewCity(sc.next());
                     break;
-                case 2:viewState();
+                case 2:
+                    System.out.println("Enter state name: ");
+                    viewState(sc.next());
                     break;
-                case 3:viewZipCode();
+                case 3:
+                    System.out.println("Enter zip code: ");
+                    viewZipCode(sc.nextInt());
                     break;
+                default:
+                    viewContactBy();
             }
-
         }
-        public void display () {
-            System.out.println(details);
+    void showContacts(ArrayList addressBook) {
+        System.out.println("Contacts: ");
+        for (Object p : addressBook) {
+            ContactDetails person = (ContactDetails) p;
+            System.out.println(person);
         }
+    }
+    public void chooseAddressBook() {
+        System.out.println("""
+        Press 1 to add AddressBook
+        Press 2 to select AddressBook""");
 
+        int option = sc.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter name for AddressBook");
+                String AddressBookName = sc.next();
+                ArrayList<ContactDetails> AddressBook = new ArrayList();
+                addressBookList.put(AddressBookName, AddressBook);
+                System.out.println("new AddressBook created");
+                currentAddressBook = addressBookList.get(AddressBookName);
+                currentAddressBookName = AddressBookName;
+                break;
+            case 2:
+                System.out.println(addressBookList.keySet());
+                System.out.println("enter name of address book");
+                String addressBookName = sc.next();
+
+                for (String key : addressBookList.keySet()) {
+                    if (key.equalsIgnoreCase(addressBookName)) {
+                        currentAddressBook = addressBookList.get(key);
+                        currentAddressBookName = key;
+                    }
+                }
+                System.out.println("current AddressBook is " + currentAddressBookName);
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void display () {
+            System.out.println(currentAddressBook);
+        }
 }
